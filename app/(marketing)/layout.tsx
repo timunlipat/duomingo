e2097a15954d7a7308246bbf2a2c31c1ceee9133
Zamil <zamilaziz92@gmail.com>
2024-06-25 17:31:19 +0800

    Added clerk

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,19 @@
+import { Footer } from "./footer";
+import { Header } from "./header";
+
+type Props = {children: React.ReactNode}
+
+const MarketingLayout = ({ children }: Props) => {
+    return(
+        <div className="min-h-screen flex flex-col">
+            <Header/>
+            <link rel="icon" href="/mascot.svg" />
+            <main className="flex-1 flex flex-col items-center justify-center">
+                {children}
+            </main> 
+            <Footer/>
+        </div>
+    )
+}
+
+export default MarketingLayout;
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="danger">
+        Testing 123
+      </Button>
+    </div>
+  )
+}
