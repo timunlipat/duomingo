bd69124fb3fa9e3bdab1fe537b1c410ee200a5bd
Zamil <zamilaziz92@gmail.com>
2024-06-29 13:01:24 +0800

    Add wrappers to learn page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,22 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
+
 const Learn = () => {
     return(
-        <div>
-            Learn
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                <UserProgress
+                    activeCourse={{ title: "Korean", imageSrc:"/korea.png"}}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                />
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Korean"/>
+            </FeedWrapper>
         </div>
     )
 }
